57b14eef882ec720f02ee706c354d6eb55409583
Nina B <ngbuscemi@gmail.com>
2024-02-08 15:18:22 -0500

    added a functioin for validating color

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 const inquirer = require('inquirer'); // Provides command line user interfaces
-const fs = requrire('fs'); // Used to read from and write to files
+const fs = require('fs'); // Used to read from and write to files
 const { Shape, Triangle, Square, Circle } = require('./lib/shapes.js');
 
-// Function to prompt the user for input
-inquirer.prompt([
-        {
-            type: 'input',
-            name: 'text',
-            message: 'Enter up to three caharcters:',
-            validate: function (input) {
-                return input.length <=3 ? true: 'Please enter up to three characters.';
-            }
-        },
-        {
-            type: 'input',
-            name: 'textColor',
-            message: 'Enter the text color:',
-            validate: function (input) {
-                const isValid = validateColor(input);
-                return isValid;
-            }
-        },
-        {
-            name: 'shape',
-            type: 'list',
-            message: 'Choose a Preferred shape:',
-            choices: ['circle', 'triangle', 'square']
-        },
-        {
-            name: 'shapecolor',
-            type: 'input',
-            message: 'Enter a color keyword for hexideacimal for shape color'
-        },
-    ]);
+function validateColor(color) {
+  const regex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+  return regex.test(color);
+}
+
+async function getUserInput() {
+  const answers = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters:',
+      validate: function (input) {
+        return input.length <= 3 ? true : 'Please enter up to three characters.';
+      }
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter the text color:',
+      validate: function (input) {
+        const isValid = validateColor(input);
+        return isValid ? true : 'Please enter a valid color.';
+      }
+    },
+    {
+      name: 'shape',
+      type: 'list',
+      message: 'Choose a preferred shape:',
+      choices: ['circle', 'triangle', 'square']
+    },
+    {
+      name: 'shapeColor',
+      type: 'input',
+      message: 'Enter a color keyword or hexadecimal for shape color:'
+    }
+  ]);
+
+  return answers;
+}
+
